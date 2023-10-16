0528896c3d8c1b2a941822d592936740a2b39c5b
Killian BALLAND <kballand@ippon.fr>
2023-10-16 11:42:47 +0000

    fix: Fix duplicated orders

--- models/staging/base/base_orders.sql
+++ models/staging/base/base_orders.sql
@@ -1,7 +1,18 @@
+WITH first_row AS (
+    SELECT
+        identifier,
+        restaurant_identifier,
+        payment_method,
+        amount,
+        created_at,
+        ROW_NUMBER() OVER (PARTITION BY identifier ORDER BY created_at DESC) AS row_nb
+    FROM {{ source('snowflake', 'orders') }}
+)
 SELECT
     identifier,
     restaurant_identifier,
     payment_method,
     amount,
     created_at
-FROM {{ source('snowflake', 'orders') }}
+FROM first_row
+WHERE row_nb = 1
