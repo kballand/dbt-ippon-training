fcbf65c2d9fc8b6b8dc2fd1d221b9e156f81fb5c
Killian BALLAND <kballand@ippon.fr>
2023-10-16 12:58:06 +0000

    fix: Fix format

--- models/staging/stg_restaurants__turnover_per_payment_method.sql
+++ models/staging/stg_restaurants__turnover_per_payment_method.sql
@@ -1,7 +1,9 @@
+{% set payment_methods = dbt_utils.get_column_values(ref('base_orders'), 'payment_method') %}
+
 SELECT
     r.identifier AS restaurant_identifier,
     r.name AS restaurant_name,
-    {% for payment_method in dbt_utils.get_column_values(ref('base_orders'), 'payment_method') %}
+    {% for payment_method in payment_methods %}
     SUM(CASE WHEN o.payment_method = '{{payment_method}}' THEN o.amount ELSE 0 END) AS {{payment_method}}_turnover{{ "," if not loop.last }}
     {% endfor %}
 FROM {{ ref('base_orders') }} AS o
