6ef7ea838676a8bb3be5558b89c13677ce7a0cde
Killian BALLAND <kballand@ippon.fr>
2023-10-17 09:15:49 +0000

    feat: Make base orders incremental

--- models/staging/base/base_orders.sql
+++ models/staging/base/base_orders.sql
@@ -1,20 +1,12 @@
-{{ config(materialized = "table") }}
+{{ config(materialized = "incremental") }}
 
-WITH first_row AS (
-    SELECT
-        identifier,
-        restaurant_identifier,
-        payment_method,
-        amount,
-        created_at,
-        ROW_NUMBER() OVER (PARTITION BY identifier ORDER BY created_at DESC) AS row_nb
-    FROM {{ mockable_source('orders') }}
-)
 SELECT
     identifier,
     restaurant_identifier,
     payment_method,
     amount,
     created_at
-FROM first_row
-WHERE row_nb = 1
+FROM {{ mockable_source('orders') }}
+{% if is_incremental() %}
+WHERE created_at > (SELECT MAX(created_at) FROM {{ this }})
+{% endif %}
